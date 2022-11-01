4107db384d15839a4b9aa53b66ebe6f994fe1136
yunjin5450 <yunjin5450@gmail.com>
2022-11-02 00:09:19 +0900

    11.02 12:08 "dev풀받기위한 커밋" - 최윤진

--- controllers/likes.controller.js
+++ controllers/likes.controller.js
@@ -5,28 +5,28 @@ class LikesController {
 
     //내가 찜한 숙소 조회
     getLikes = async (req, res, next) => {
-        //try {
+        try {
             const {memberId} = req.params
             const getLikes = await this.likesService.getLikes(memberId)
             res.status(200).json({data: getLikes})            
-        // } catch(error) {
-        // res.status(400).json({error: error.message})
-        // }
+        } catch(error) {
+        res.status(400).json({error: error.message})
+        }
     }
 
 
     //숙소 찜하기 (누르기, 수락, 취소)  
     updateLikes = async (req, res, next) => {
-        //try {
+        try {
             const { accId } = req.params
             const { memberId } = res.locals.user
             const updateLikes = await this.likesService.updateLikes(accId, memberId)
             res.status(200).json({message : updateLikes})
-        // } catch(error) {
-        // res.status(400).json({error: error.message})
-        // }   
+        } catch(error) {
+        res.status(400).json({error: error.message})
+        }   
     } 
     
 }
 
-module.exports = LikesController;
+module.exports = LikesController
